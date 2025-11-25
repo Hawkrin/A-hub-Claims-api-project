e61c7f60b3cba8687a9126fe742f490cad28ed22
Malcolm Liljedahl <malcolm.liljedahl@agria.se>
2025-11-25 14:03:05 +0100

    changes

--- ASP.Claims.API/obj/Release/net9.0/ASP.Claims.API.AssemblyInfo.cs
+++ ASP.Claims.API/obj/Release/net9.0/ASP.Claims.API.AssemblyInfo.cs
@@ -12,7 +12,7 @@ using System;
 using System.Reflection;
 
 [assembly: System.Reflection.AssemblyCompanyAttribute("ASP.Claims.API")]
-[assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
+[assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
 [assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+1da6a9ed5692e2a2856545cadd31f767454cb4c1")]
 [assembly: System.Reflection.AssemblyProductAttribute("ASP.Claims.API")]
