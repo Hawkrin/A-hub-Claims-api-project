58017db13bb854b2ba972a7075b85afb3adc8eb9
Malcolm Liljedahl <malcolm.liljedahl@agria.se>
2025-12-23 15:41:32 +0100

    GetById comso func updated

--- ASP.Claims.API/Infrastructures/Repositories/CosmosDbClaimRepository.cs
+++ ASP.Claims.API/Infrastructures/Repositories/CosmosDbClaimRepository.cs
@@ -4,23 +4,20 @@ using ASP.Claims.API.Domain.Enums;
 using ASP.Claims.API.Resources;
 using FluentResults;
 using Microsoft.Azure.Cosmos;
+using Newtonsoft.Json.Linq;
 using System.Net;
 
 namespace ASP.Claims.API.Infrastructures.Repositories;
 
-public class CosmosDbClaimRepository : IClaimRepository
+public class CosmosDbClaimRepository(Container container) : IClaimRepository
 {
-    private readonly Container _container;
-
-    public CosmosDbClaimRepository(Container container)
-    {
-        _container = container;
-    }
+    private readonly Container _container = container;
 
     public async Task<IEnumerable<Claim>> GetAll()
     {
         var query = _container.GetItemQueryIterator<Claim>("SELECT * FROM c");
         var results = new List<Claim>();
+
         while (query.HasMoreResults)
         {
             var response = await query.ReadNextAsync();
@@ -34,6 +31,7 @@ public class CosmosDbClaimRepository : IClaimRepository
         var query = _container.GetItemQueryIterator<Claim>(
             $"SELECT * FROM c WHERE c.Type = {(int)type}");
         var results = new List<Claim>();
+
         while (query.HasMoreResults)
         {
             var response = await query.ReadNextAsync();
@@ -46,8 +44,25 @@ public class CosmosDbClaimRepository : IClaimRepository
     {
         try
         {
-            var response = await _container.ReadItemAsync<Claim>(id.ToString(), new PartitionKey(id.ToString()));
-            return response.Resource;
+            // Read as JObject to inspect discriminator
+            var response = await _container.ReadItemAsync<JObject>(id.ToString(), new PartitionKey(id.ToString()));
+            var json = response.Resource;
+
+            // Get discriminator value
+            var claimTypeString = json["ClaimType"]?.ToString();
+            if (string.IsNullOrEmpty(claimTypeString))
+                throw new Exception("ClaimType discriminator missing!");
+
+            var claimType = Enum.Parse<ClaimType>(claimTypeString);
+
+            // Deserialize to correct type
+            return claimType switch
+            {
+                ClaimType.Property => json.ToObject<PropertyClaim>(),
+                ClaimType.Vehicle => json.ToObject<VehicleClaim>(),
+                ClaimType.Travel => json.ToObject<TravelClaim>(),
+                _ => throw new Exception($"Unknown ClaimType: {claimTypeString}"),
+            };
         }
         catch (CosmosException ex) when (ex.StatusCode == HttpStatusCode.NotFound)
         {
