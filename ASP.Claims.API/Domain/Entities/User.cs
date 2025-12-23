decd649c1fe0ad1551fd8d940f33d0431867db39
Malcolm Liljedahl <malcolm.liljedahl@agria.se>
2025-12-23 14:16:37 +0100

    probably fixed issue with registration

--- ASP.Claims.API/Domain/Entities/User.cs
+++ ASP.Claims.API/Domain/Entities/User.cs
@@ -1,9 +1,11 @@
 ﻿using ASP.Claims.API.Domain.Enums;
+using Newtonsoft.Json;
 
 namespace ASP.Claims.API.Domain.Entities;
 
 public class User
 {
+    [JsonProperty("id")]
     public required string Id { get; set; }
     public required string Username { get; set; }
     public required string Password { get; set; }
